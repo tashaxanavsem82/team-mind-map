33ef1152e187f4c26d6ea6b04f00ffa6a01f3efc
tashaxanavsem82 <swgwull@hotmail.com>
2025-11-03 04:40:47 -0600

    feat: initial commit for src/App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to Team Mind Map</h1>
+      <p>Create and collaborate on your mind maps!</p>
+    </div>
+  );
+}
+
+export default App;
